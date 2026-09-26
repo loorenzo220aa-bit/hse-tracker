73b85be18c6d74403c95669f3324a27bb9a14449
loorenzo220aa-bit <loorenzo220aa-bit@users.noreply.github.com>
2026-09-26 14:05:25 +0300

    feat: network indicator, last-month memory, loading spinner, portrait lock, back button, SRI, touch targets

--- android/app/src/main/java/com/loor/hsetracker/MainActivity.java
+++ android/app/src/main/java/com/loor/hsetracker/MainActivity.java
@@ -2,4 +2,13 @@ package com.loor.hsetracker;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        try {
+            android.webkit.WebView w = getBridge().getWebView();
+            if (w != null && w.canGoBack()) { w.goBack(); return; }
+        } catch (Exception e) {}
+        super.onBackPressed();
+    }
+}
